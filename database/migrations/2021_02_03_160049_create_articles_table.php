98a65f20eb7fa916ce94cb45b697e6ded22697f0
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-04 10:54:19 +0900

    article_table_createを修正
    保存し忘れていた部分をrollbackしてup

--- database/migrations/2021_02_03_160049_create_articles_table.php
+++ database/migrations/2021_02_03_160049_create_articles_table.php
@@ -15,6 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('body');
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
