2d76398ee0f740a4276150d8f3b13437b4dff042
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-07 09:15:14 +0900

    投稿画面編集(4-3)
    nav.bblade.php route編集
    form.blade.php フォーム作成include
    create.blade.php view作成

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,26 @@
+@extends('app')
+
+@section('title', '新規投稿')
+
+@section('content')
+
+ @include('nav')
+ <div class='container'>
+   <div class='row'>
+     <div class='col-12'>
+       <div class='card mt-3'>
+         <div class='card body pt-0'>
+           @include('error_card_list')
+           <div class='card-text'>
+             <form action="{{ route('articles.store') }}">
+               @include('articles.form')
+               <button type='submit' class='btn blue-gradient btn-block'>投稿する</button>
+             </form>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+
+@endsection
---
+++ resources/views/articles/form.blade.php
@@ -0,0 +1,9 @@
+@csrf
+<div class='mg-form'>
+  <label>タイトル</label>
+  <input type="text" name='title' class='form-control' required value='{{ ord("title") }}'>
+</div>
+<div class='form-group'>
+  <label></label>
+  <textarea name="body" required class='form-control' rows="16" placeholder='本文'></textarea>
+</div>
