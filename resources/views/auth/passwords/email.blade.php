4b43e2eaad4c685830c1e70d39f924ba2240d0a6
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-09 00:03:07 +0900

    パスワード設定のviewなど(6-2)

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,43 @@
+@extends('app')
+
+@section('title', 'パスワード再設定')
+
+@section('content')
+
+ @include('nav')
+ <div class='container'>
+   <div class='row'>
+      <div class="mx-auto col col-12 col-sm-12 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">パスワード再設定</h2>
+
+            @include('error_card_list')
+
+            @if(session('status'))
+              <div class="card-text alert alert-success">
+                {{ session('status') }}
+              </div>
+            @endif
+
+            <div class="card-text">
+              <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class='md-form'>
+                <label for="email">メールアドレス</label>
+                  <input type="text" id="email" name='email' class='form-control' required>
+                </div>
+                
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="sublit">メール送信</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
