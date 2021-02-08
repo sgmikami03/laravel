7dcac465d217aedd27606aa0a184c5bb4a629e6e
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-08 19:08:03 +0900

    destroy機能追加
    ArticleController修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -71,5 +71,11 @@ class ArticleController extends Controller
         $article->fill($request->all())->save();
         return redirect()->route('articles.index');
     }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
 }
 
