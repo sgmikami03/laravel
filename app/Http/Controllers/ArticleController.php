3809d3715675b2bd6bc9e54d588c3439fdecbabe
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-18 11:31:18 +0900

    サンプルデータ削除
    相当前のやつ
    忘れてた

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -14,38 +14,6 @@ class ArticleController extends Controller
     }
     
     public function index(){
-        /*$articles = [
-            (object)[
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文',
-                'created_at' => now(),
-                'user' =>(object)[
-                    'id' => 1,
-                    'name' => 'ユーザー名1'
-                ],
-            ],
-            (object)[
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文',
-                'created_at' => now(),
-                'user' =>(object)[
-                    'id' => 2,
-                    'name' => 'ユーザー名2'
-                ],
-            ],
-            (object)[
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文',
-                'created_at' => now(),
-                'user' =>(object)[
-                    'id' => 3,
-                    'name' => 'ユーザー名3'
-                ],
-            ],
-        ];*/
 
         $articles = Article::all()->sortByDesc('created_at');
 
