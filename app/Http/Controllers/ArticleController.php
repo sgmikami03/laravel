c49bb3aae5f3427e1bc4974ef0f6c53661d007c6
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-19 12:21:16 +0900

    タグの挿入contllorer編集(8-8)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ArticleRequest;
 use App\Article;
+use App\Tag;
 
 class ArticleController extends Controller
 {
@@ -31,6 +32,11 @@ class ArticleController extends Controller
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
+
+        $request->tags->each(function($tagName)use($article){
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
     }
 
     public function edit(Article $article)
