10e4ab52e6a7f2381bf9545760772282bbd1fe94
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-22 11:20:23 +0900

    9-3

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button
+          class="btn-sm shadow-none border border-primary p-2"
+          :class="buttonColor"
+        >
+          <i
+          class="mr-1"
+          :class="buttonIcon"
+          ></i>
+          {{ buttonText }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isFollowBy: false,
+        }
+    },
+    computed: {
+        buttonColor(){
+            return this.isFollowBy
+              ?'bg-primary text-white'
+              :'bg-white'
+        },
+        buttonIcon(){
+            return this.isFollowBy
+            ?'fas fa-user-check'
+            :'fas fa-user-plus'
+        },
+        buttonText(){
+            return this.isFollowBy
+            ?'フォロー中'
+            :'フォロー'
+        }
+    }
+}
+</script>
