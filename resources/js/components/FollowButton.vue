4f9a17702e136b5e5ea61533b86abbba6177a132
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-02-25 23:33:14 +0900

    9-8

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -3,6 +3,7 @@
         <button
           class="btn-sm shadow-none border border-primary p-2"
           :class="buttonColor"
+          @click="clickFollow"
         >
           <i
           class="mr-1"
@@ -19,6 +20,13 @@ export default {
         initialIsFollowedBy:{
             type: Boolean,
             default: false,
+        },
+        authorized:{
+            type: Boolean,
+            default: false,
+        },
+        endpoint: {
+            type: String,
         }
     },
     data() {
@@ -41,6 +49,26 @@ export default {
             return this.isFollowedBy
             ?'フォロー中'
             :'フォロー'
+        }
+    },
+    methods: {
+        clickFollow() {
+            if(!this.authorized){
+                alert('フォロー機能はログイン中のみ使用できます')
+                return
+            }
+
+            this.isFollowedBy
+            ? this.unfollow()
+            : this.follow()
+        },
+        async follow(){
+            const responce = await axios.put(this.endpoint)
+            this.isFollowedBy = true
+        },
+        async unfollow(){
+            const responce = await axios.delete(this.endpoint)
+            this.isFollowedBy = false
         }
     }
 }
